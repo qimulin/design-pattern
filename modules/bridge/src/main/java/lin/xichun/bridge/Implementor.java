a603ccae910865c7489e60109092e7aaccb7429a
linxichun <linxichun@citydo.com.cn>
2021-04-28 11:28:18 +0800

    README文件提交

--- modules/bridge/src/main/java/lin/xichun/bridge/Implementor.java
+++ modules/bridge/src/main/java/lin/xichun/bridge/Implementor.java
@@ -4,6 +4,8 @@ package lin.xichun.bridge;
  * Created by Lin.XiChun on 2018/10/24.
  */
 public interface Implementor {
+    /** 操作实现1 */
     void operationImpl();
+    /** 操作实现2 */
     void operationImpl2();
 }
